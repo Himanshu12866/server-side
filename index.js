069bce08c3fefee084bf0fc741c81141567880d9
Himanshu12866 <manshu010m@gmail.com>
2024-09-23 17:47:30 +0530

    Fetched data successfully from the database with help of express.js

--- index.js
+++ index.js
@@ -61,23 +61,54 @@
 // app.listen(4200)
 // console.log(`Server started at "http://127.0.0.1:4200"`)
 
-var http = require("http")
-var mongoClient = require("mongodb").MongoClient;
-var url = "mongodb://localhost:27017";
-var app = http.createServer((req, res) => {
-	res.writeHead(200, { "content-type": "application/json" })
+// var http = require("http")
+// var mongoClient = require("mongodb").MongoClient;
+// var url = "mongodb://localhost:27017";
+// var app = http.createServer((req, res) => {
+// 	res.writeHead(200, { "content-type": "application/json" })
+// 	mongoClient.connect(url).
+// 		then(clientObj => {
+// 			var db = clientObj.db("newcollection")
+
+// 			db.collection("newcolletion").find({}).toArray()
+// 				.then(
+// 					document => {
+// 						res.write(JSON.stringify(document));
+// 						res.end()
+// 					}
+// 				)
+// 		})
+// })
+// app.listen(5000)
+// console.log(`Server started at "http://127.0.0.1:5000"`)
+
+
+const express = require('express');
+const mongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const app = express();
+
+
+app.get("/", (req, res) => {
+	res.send("<h1>Welcome to the API home page</h1>")
+})
+app.get("/men", (req, res) => {
+	res.send("<h2>Welcome to the Mens Fashion</h2>")
+})
+app.get("/products", (req, res) => {
 	mongoClient.connect(url).
 		then(clientObj => {
-			var db = clientObj.db("newcollection")
-
-			db.collection("newcolletion").find({}).toArray()
-				.then(
-					document => {
-						res.write(JSON.stringify(document));
-						res.end()
-					}
-				)
+			var db = clientObj.db("admin")
+			db.collection("products").find({}).toArray().then(
+				document => {
+					res.send(document)
+					res.end()
+				}
+			)
 		})
+})
+app.get("*" , (req,res) => {
+	res.send("<h1>404 Error</h1>")
 })
 app.listen(5000)
 console.log(`Server started at "http://127.0.0.1:5000"`)
