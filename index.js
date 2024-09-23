bbc268e6e6ca8770f597eb411ca4d45040fc8679
Himanshu12866 <manshu010m@gmail.com>
2024-09-23 16:12:18 +0530

    Modified

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
+
 var os = require("os");		// os is a module 
 	os.cpus();
-=======
-var os = require("os");		// os is a module 
-	os.cpus();
->>>>>>> e1f5227629409c9cb42fc14058270e3e95830060
+
