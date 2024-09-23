0ad434a23424512eff299837ddea4d8049ea26fb
Himanshu12866 <manshu010m@gmail.com>
2024-09-23 16:50:23 +0530

    successfully printed JSON file using the nodejs server

--- index.js
+++ index.js
@@ -7,12 +7,36 @@
 // 	console.log(os.release())
 
 
-var http = require("http")
-var app = http.createServer((request, response) => {
-	response.writeHead(200, { "content-type": "text/html" })
+// var http = require("http")
+// var app = http.createServer((request, response) => {
+// 	response.writeHead(200, { "content-type": "text/html" })
 
-	response.write("<h2>Welcome to the Node JS Server</h2>");
-	response.end();
+// 	response.write("<h2>Welcome to the Node JS Server</h2>");
+// 	response.end();
+// })
+// app.listen(4000)
+// console.log(`Server Started... "http://127.0.0.1:4000"`)
+
+
+let http = require("http")
+let app = http.createServer((req, res) => {
+	res.writeHead(200, { "content-type": "application/json" });
+	res.write(JSON.stringify(
+		[
+		{ 
+			"name": "Himanshu", 
+			"lastName": "Mishra",
+			 "age": 22
+			 }, 
+			 { 
+				"name": "Hema",
+				 "lastName": "Mishra",
+				  "age": 22
+				 }
+				]
+			)
+		)
+		res.end()
 })
-app.listen(4000)
-console.log(`Server Started... "http://127.0.0.1:4000"`)
+app.listen(4500)
+console.log(`Server started at .... "http://127.0.0.1:4500`)
