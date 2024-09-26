ab4aac33e411709af97ec75e12b42471b20379bc
Himanshu12866 <manshu010m@gmail.com>
2024-09-26 17:07:31 +0530

    Modified

--- api.js
+++ api.js
@@ -3,12 +3,54 @@ const mongoclient = require('mongodb').MongoClient;
 const cors = require('cors');
 const url = "mongodb://127.0.0.1:27017"
 const app = express()
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cors())
 
-app.get("/" , (req,res) => {
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to the API Server");
+    res.end()
+})
+app.get("/categories", (req, res) => {
     mongoclient.connect(url).then(clientObj => {
         let db = clientObj.db("admin")
+        db.collection("categories").find({}).toArray().then(document => {
+            res.send(document)
+            res.end()
+        })
     })
 })
+
+app.get("/products", (req, res) => {
+    mongoclient.connect(url).then(clientObj => {
+        let db = clientObj.db("admin")
+        db.collection("products").find({}).toArray().then(document => {
+            res.send(document)
+            res.end()
+        })
+    })
+})
+
+app.post("/add-products", (req, res) => {
+    let product = {
+        id: parseInt(req.body.id),
+        title: req.body.title,
+        price: parseInt(req.body.price),
+        description: req.body.description,
+        image: req.body.image,
+        rating: {
+            rate: parseInt(req.body.rate),
+            count: parseInt(req.body.count)
+        }
+    }
+
+    mongoclient.connect(url).then(clientObj => {
+        let db = clientObj.db("admin")
+        db.collection("products").insertOne(product).then(() => {
+            res.redirect("/products")
+            res.end();
+        })
+    })
+})
+app.listen(1000)
+console.log("Server started at  http://127.0.0.1:1000")
